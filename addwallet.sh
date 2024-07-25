d4bfcda7fb176a6a7f5456cfb8982f6b3c09d160
Designer <54180975+Designer0101@users.noreply.github.com>
2024-07-26 02:33:46 +0300

    Update addwallet.sh

--- addwallet.sh
+++ addwallet.sh
@@ -2,15 +2,12 @@
 
 read -p "Wallet Name: " walletname
 
-echo "0gchaind keys add $walletname --eth"
 output=$(0gchaind keys add $walletname --eth)
 address=$(echo "$output" | awk -F ': ' '/address/ {print $2}')
-mnemonic=$(echo "$output" | awk '/It is the only way/ {for (i=6; i<=NF; i++) print $i}')
+mnemonic=$(echo "$output" | awk '/It is the only way/ {for (i=6; i<=NF; i++) printf "%s ", $i}')
 
-echo "0gchaind keys unsafe-export-eth-key $walletname"
 privatekey=$(0gchaind keys unsafe-export-eth-key $walletname)
 
-echo "0gchaind debug addr \$(0gchaind keys show $walletname -a) | grep 'Address (hex):' | awk -F ': ' '{print \"0x\" \$2}'"
 publickey=$(0gchaind debug addr $(0gchaind keys show $walletname -a) | grep 'Address (hex):' | awk -F ': ' '{print "0x" $2}')
 
 echo "address: $address"
