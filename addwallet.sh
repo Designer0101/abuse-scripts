c2823c6a4dd2a3d92f69c1a9f95a7d5bf1cbeb1c
Designer <54180975+Designer0101@users.noreply.github.com>
2024-07-26 02:37:34 +0300

    Update addwallet.sh

--- addwallet.sh
+++ addwallet.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-read -p "Wallet Name: " walletname
+read -p "Walletname: " walletname
 
 output=$(0gchaind keys add $walletname --eth)
-address=$(echo "$output" | awk -F ': ' '/address/ {print $2}')
-mnemonic=$(echo "$output" | awk '/It is the only way/ {for (i=6; i<=NF; i++) printf "%s ", $i}')
+address=$(echo "$output" | grep -o -E 'address: [a-zA-Z0-9]+' | awk -F ': ' '{print $2}')
+mnemonic_start=$(echo "$output" | awk '/It is the only way/ {print NR}')
+mnemonic_end=$((mnemonic_start + 2))
+mnemonic=$(echo "$output" | awk -v start=$mnemonic_start -v end=$mnemonic_end 'NR >= start && NR <= end {print $0}' | tr '\n' ' ')
 
 privatekey=$(0gchaind keys unsafe-export-eth-key $walletname)
 
 publickey=$(0gchaind debug addr $(0gchaind keys show $walletname -a) | grep 'Address (hex):' | awk -F ': ' '{print "0x" $2}')
 
-echo "address: $address"
-echo "mnemonic: $mnemonic"
-echo "privatekey: $privatekey"
-echo "publickey: $publickey"
+echo "$address"
+echo "$mnemonic"
+echo "$privatekey"
+echo "$publickey"
